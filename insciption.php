f1c224c30f91d6e2640c4ffa375f385ddf6bdf80
Jey <j.bensaudepro@gmail.com>
2023-01-04 10:14:41 +0100

    Update insciption.php

--- insciption.php
+++ insciption.php
@@ -59,7 +59,7 @@ if(!empty($_POST)){
             </div>
         
     </header>
-        <h1>Inscription</h1>
+        <h1>Inscription v2</h1>
         <form method="post">
             <div>
                 <label for="pseudo">Pseudo:</label>
