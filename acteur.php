3a7a0f9a01c67ccd87e67318b0efadb9616ef877
Jey <j.bensaudepro@gmail.com>
2023-02-07 14:48:49 +0100

    Fonction Like/Dislike
    Création de la fonction like/dislike avec utilisation de l'includes/action.php -> function sqlGetNBvote

--- acteur.php
+++ acteur.php
@@ -1,6 +1,9 @@
 <?php
 //Je démarre la session
 session_start();
+
+include_once './includes/action.php';
+
 //Je verifie si j'ai un id    "||"
 if(!isset($_GET["id_acteur"])){
     //Je n'ai pas d'id
@@ -12,12 +15,13 @@ if(!isset($_SESSION["user"])){
     exit;
 }
 //je recup l'id et l'id_acteur
-
 $iduser = $_SESSION["user"]["id"];
 $id = $_GET["id_acteur"];
 $date_creation = date('Y-m-d H:i:s');
+
+//---------------------J'affiche mon acteur selon l'id_acteur-------------//
 //Je me connecte à la base de donnée
-require_once "includes/connect.php";
+require "includes/connect.php";
 //Je vais chercher l'article dans ma bd
 //J'écris la request
 $sql = "SELECT * FROM `acteur` WHERE `id_acteur` = :id";
@@ -30,9 +34,26 @@ $request -> execute();
 //Je récupère l'article
 $acteur = $request->fetch();
 
-// var_dump($iduser);
-// var_dump($id);
+var_dump($id);
+var_dump($iduser);
 
+//---------------------Gestion des votes existants
+
+// 1- Nombre de votes
+$NBVotes = sqlGetNBVotes($db,$id);
+
+// 2- Récup du vote de l'utilisateur courrant s'il existe
+$voteUser = null;
+$reqSQL = "SELECT vote FROM vote WHERE id_user=$iduser and id_acteur=$id";
+echo $reqSQL;
+$resultVoteUser = $db->prepare($reqSQL);
+$resultVoteUser->execute();
+if($result = $resultVoteUser->fetch()){
+    $voteUser = $result["vote"];
+    echo "eee $voteUser eee";
+}
+echo "ee $voteUser ee";
+//---------------------Poster le commentaire---------------------//
     if(isset($_POST["submit_commentaire"])){
         if(isset($_POST["post"]) AND !empty($_POST["post"])){
             $commentaire = htmlspecialchars($_POST["post"]);
@@ -43,56 +64,32 @@ $acteur = $request->fetch();
         }else{
             $c_msg = "<span style='color:red'>Erreur: Vous devez écrire un commentaire pour le poster</span>";
         }
-    }
-//fonction like/dislike
-function vote($iduser, $id, $vote){
-
-    $iduser = $_SESSION["user"]["id"];
-    $id = $_GET["id_acteur"];
-    require_once('includes/connect.php');
-    
-    if($vote == 1){
-        echo "like";
-        // Si ça n'existe pas  => l'association id_user et id_acteur pas dans la table VOTE
-        if(isset($id, $iduser)){
-            $inslike = $db->prepare('INSERT INTO `vote`(`id_user`, `id_acteur`, `vote`) VALUES (:iduser, :idacteur, :vote)');
-            $inslike->bindValue( ":iduser", $iduser);
-            $inslike->bindValue( ":idacteur", $id);
-            $inslike->bindValue( ":vote", $vote);
-            $inslike->execute();
-        }else{
-            echo " j'existe pas";
+    }else{
+//--------------------fonction like/dislike-----------------------------//
+        if(isset($_POST["vote"])){
+            // Récupérer le vote :
+            $nouveauVote = $_POST["vote"];
+            if($voteUser == null){
+                $inslike = $db->prepare('INSERT INTO `vote`(`id_user`, `id_acteur`, `vote`) VALUES (:iduser, :idacteur, :vote)');
+                $inslike->bindValue( ":iduser", $iduser);
+                $inslike->bindValue( ":idacteur", $id);
+                $inslike->bindValue( ":vote", $nouveauVote);
+                $inslike->execute();
+                
+            }else{
+                
+                $inslike = $db->prepare('UPDATE `vote` SET `vote`=:vote WHERE `id_user` = :iduser AND `id_acteur`=:idacteur');
+                $inslike->bindValue( ":iduser", $iduser);
+                $inslike->bindValue( ":idacteur", $id);
+                $inslike->bindValue( ":vote", $nouveauVote);
+                $inslike->execute();
+            }
+            $voteUser = $nouveauVote;
+            $NBVotes = sqlGetNBVotes($db,$id);
         }
-        
-
-        // INSERT INTO `vote`(`id_user`, `id_acteur`, `vote`) VALUES ( 1,1, $vote)
-        // Si ça existe déja 
-        //UPDATE
     }
-    elseif($vote == -1){
-        echo "Dislike";
-    }
-}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//---------------------Afficher les commentaires---------------------//
 //requête des commentaires / 'ORDER BY `id_post` desc' pour faire remonter le commentaire le plus rescend
 $commentaires = $db->prepare('SELECT * FROM `post`, `account` WHERE post.id_user = account.id_user AND `id_acteur` = ? ORDER BY `id_post`desc');
 $commentaires-> execute(array($id));
@@ -111,16 +108,46 @@ $titrepage = "$acteur[acteur]";
                 <p><?php echo $acteur["description"]?></p>
             </div>
             <div class="acteur_Like">
-                 <a href="acteur.php?vote=1&id_acteur=<?= $id ?>"><img class="icon_like" src="ressources/like_dislike/like_off.png" alt=""></a>
-                <br>
-                <a href="acteur.php?vote=-1&id_acteur=<?= $id ?>"><img class="icon_like" src="ressources/like_dislike/dislike_off.png" alt=""></a>
-                <br>
-                <a href="acteur.php?vote=0&id_acteur=<?= $id ?>">Je n'aime plus</a>
-                            <?php 
-                        if (isset($_GET['vote']))
-                            vote(1,1, $_GET['vote']);
+                <form method="post">
+
+
+                <?
+                        switch($voteUser){
+                            case 1:
+                                // A voté "Like" : afficher like vert
+                                echo '<div class="icon_like iconLikeOn"></div>';                                
+                                echo $NBVotes;
+                                echo '<input class="icon_like iconDislikeOff" type="submit" value="-1" name="vote" />'; 
+                                break;
+                            case -1:
+                                echo '<input class="icon_like iconLikeOff" type="submit" value="1" name="vote" />'; 
+                                echo $NBVotes;
+                                echo '<div class="icon_like iconDislikeOn"></div>';     
+                                break;
+                            default:
+                                echo '<input class="icon_like iconLikeOff" type="submit" title="LIKE" value="1" name="vote" />';
+                                echo $NBVotes;
+                                echo '<input class="icon_like iconDislikeOff" type="submit" title="DISLIKE" value="-1" name="vote" />'; 
+                                break;
+                        }                 
+
+
+
+                ?>
+
+                    <!--<a href="acteur.php?vote=1&id_acteur=<?= $id ?>"></a>
                     
-                            ?>
+                  
+                    
+
+
+                    <br>
+                    <a href="acteur.php?vote=-1&id_acteur=<?= $id ?>"><img class="icon_like" src="ressources/like_dislike/dislike_off.png" alt=""></a>
+                    
+                    <br>
+                    <a href="acteur.php?vote=0&id_acteur=<?= $id ?>">Je n'aime plus</a>-->
+                </form>
+                            
             </div>
         </article>
         <article class="bloc_Commentaires">
--- includes/action.php
+++ includes/action.php
@@ -1,57 +1,14 @@
 <?php
-session_start();
-
-    function vote($iduser, $id, $vote){
-        if($vote == 1){
-            echo "like";
-            // Si ça n'existe pas  => l'association id_user et id_acteur pas dans la table VOTE
-
-            // INSERT INTO `vote`(`id_user`, `id_acteur`, `vote`) VALUES ( 1,1, $vote)
-            // Si ça existe déja 
-            //UPDATE
+    function sqlGetNBVotes($db, $id){
+        $result = 0;
+        $reqSQL = "SELECT count(vote) as 'nbvote' FROM vote WHERE id_acteur=$id and vote=1";
+        $resultNBVotes = $db->prepare($reqSQL);
+        $resultNBVotes->execute();
+        if($row = $resultNBVotes->fetch()){
+            $result = $row["nbvote"];
         }
-        else if($vote == -1){
-            echo "J'aime pas";
-        }
-}
+        return $result;
+    }
+
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<article class="bloc_Acteur">
-            <div class=""><img class="logo_png" src="" alt=""/></div>
-            <div class="">
-                <h3>ACTEUR</h3>
-                <p>descruption de l'acteur</p>
-            </div>
-            <div class="acteur_Like">
-                <a href="./action.php?vote=1">J'aime</a> (15)
-                </br>
-                <a href="./action.php?vote=-1">Je n'aime pas</a>
-            </div>
-
-            <div class="">
-                <h3>ACTEUR 2</h3>
-                <p>descruption de l'acteur</p>
-            </div>
-            <div class="acteur_Like">
-                <a href="./action.php?vote=1">J'aime</a> (15)
-                </br>
-                <a href="./action.php?vote=-1">Je n'aime pas</a>
-                <a href="./action.php?vote=0">J'enleve mon j'aime</a>
-            </div>
-        </article>
-        <?php 
-            if (isset($_GET['vote']))
-                vote(1,1, $_GET['vote']);
-          
-        ?>
-</body>
-</html>
-
